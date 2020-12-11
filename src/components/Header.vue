7a4e74723e1da579ccdd3e17d0176792bf4bd330
takashiVue <takashivue@gmail.com>
2020-12-11 13:19:19 +0900

    feat(Header.vue App.vue): Headerコンポーネントのデザインコーディング
    Headerコンポーネントをレスポンシブ含めコーディングしました。タイトル文字は変更する可能性があります。

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Header">
-    <h1>Header</h1>
+    <h1>RESAS GRAPH</h1>
   </div>
 </template>
 
@@ -9,3 +9,27 @@ export default {
   name: 'Header',
 }
 </script>
+
+<style>
+.Header {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+  padding: 0 60px;
+  text-align: left;
+  background-color: #16a085;
+  margin-bottom: 20px;
+  color: #fff;
+  box-sizing: border-box;
+  box-shadow: 0 0 8px #999;
+}
+
+@media screen and (max-width: 640px) {
+  .Header {
+    justify-content: center;
+    font-size: 12px;
+  }
+}
+</style>
